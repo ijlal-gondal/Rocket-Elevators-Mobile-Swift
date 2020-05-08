a0ba16ea1fa0bccacb9e9fb33b3624862040da9d
Ijlal Gondal <ijlal.gondal@ontariotechu.net>
2020-05-08 17:51:38 -0400

    Initial Commit

---
+++ Rocket-Elevators-Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rocket-Elevators-Mobile
+//
+//  Created by Ijlal Gondal on 2020-05-08.
+//  Copyright © 2020 Rocket Elevators. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
