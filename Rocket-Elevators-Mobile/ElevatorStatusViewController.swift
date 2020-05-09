6e2419fb7eef06dcfe5c02a110ea36f13aae6564
Ijlal Gondal <ijlal.gondal@ontariotechu.net>
2020-05-09 19:56:58 -0400

    update

---
+++ Rocket-Elevators-Mobile/ElevatorStatusViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ElevatorStatusViewController.swift
+//  Rocket-Elevators-Mobile
+//
+//  Created by Ijlal Gondal on 2020-05-08.
+//  Copyright © 2020 Rocket Elevators. All rights reserved.
+//
+
+import UIKit
+
+class ElevatorStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Elevator Status Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
